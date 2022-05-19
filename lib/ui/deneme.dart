cef546e56029e48e0e95a00476ed9e3a743c05a6
mgedikoglu <mgedik@Mustafas-MacBook-Pro.local>
2022-05-20 02:58:43 +0300

    gitignore updated

--- lib/ui/deneme.dart
+++ lib/ui/deneme.dart
@@ -1,84 +1,84 @@
-import 'package:flutter/material.dart';
-import 'package:foodstagram/model/post.dart';
-import 'package:foodstagram/util/styles.dart';
-import 'package:foodstagram/util/colors.dart';
+// import 'package:flutter/material.dart';
+// import 'package:foodstagram/model/post.dart';
+// import 'package:foodstagram/util/styles.dart';
+// import 'package:foodstagram/util/colors.dart';
 
-import 'package:foodstagram/routes/feed.dart';
-import 'package:foodstagram/ui/search.dart';
+// import 'package:foodstagram/routes/feed.dart';
+// import 'package:foodstagram/ui/search.dart';
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+// class MyStatefulWidget extends StatefulWidget {
+//   const MyStatefulWidget({Key? key}) : super(key: key);
 
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+//   @override
+//   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 
-  static const String routeName = '/feed';
-}
+//   static const String routeName = '/feed';
+// }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
+// class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+//   int _selectedIndex = 0;
 
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Search(),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
+//   static const TextStyle optionStyle =
+//       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+//   static const List<Widget> _widgetOptions = <Widget>[
+//     Text(
+//       'Index 0: Home',
+//       style: optionStyle,
+//     ),
+//     Search(),
+//     Text(
+//       'Index 2: School',
+//       style: optionStyle,
+//     ),
+//   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false, // delete back button
-        title: RichText(
-          text: TextSpan(
-            text: "Foodstagram",
-            style: appBarLogoStyle,
-          ),
-        ),
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Color.fromARGB(255, 97, 97, 97)),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         automaticallyImplyLeading: false, // delete back button
+//         title: RichText(
+//           text: TextSpan(
+//             text: "Foodstagram",
+//             style: appBarLogoStyle,
+//           ),
+//         ),
+//         backgroundColor: Color.fromARGB(255, 255, 255, 255),
+//         centerTitle: true,
+//         elevation: 0.0,
+//       ),
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home, color: Color.fromARGB(255, 97, 97, 97)),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.search),
+//             label: 'Search',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.notifications),
+//             label: 'Notifications',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.account_circle_outlined),
+//             label: 'Profile',
+//           ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         selectedItemColor: Colors.amber[800],
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
